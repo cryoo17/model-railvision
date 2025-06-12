a16f4ae72e80fba8f3b6fdfee8b85891212709a9
cryoo17 <tgun8113@gmail.com>
2025-06-12 17:21:45 +0700

    feat: API Model Prediction

--- dist/utils/env.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLOUDINARY_API_SECRET = exports.CLOUDINARY_API_KEY = exports.CLOUDINARY_CLOUD_NAME = exports.CLIENT_HOST = void 0;
-exports.CLIENT_HOST = process.env.CLIENT_HOST || "http://localhost:3001";
-exports.CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME || "";
-exports.CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY || "";
-exports.CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET || "";
